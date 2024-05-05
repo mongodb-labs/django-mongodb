e2204870a30052a514e4f73baea1eabd51953868
Emanuel Lupi <emanuel.lupi91@gmail.com>
2024-05-05 04:52:48 -0300

    dbshell: handle filenames and --shell option.

--- django_mongodb/client.py
+++ django_mongodb/client.py
@@ -5,6 +5,7 @@ from django.db.backends.base.client import BaseDatabaseClient
 
 class DatabaseClient(BaseDatabaseClient):
     executable_name = "mongosh"
+    help_options = ("--help", "-h")
 
     @classmethod
     def settings_to_cmd_args_env(cls, settings_dict, parameters):
@@ -13,13 +14,13 @@ class DatabaseClient(BaseDatabaseClient):
         args = [cls.executable_name]
         if not uri:
             host = settings_dict["HOST"]
-            port = settings_dict.get("PORT")
+            port = settings_dict["PORT"]
             if port and host:
                 host += f":{port}"
 
-            dbname = settings_dict.get("NAME", "")
-            user = settings_dict.get("USER")
-            passwd = settings_dict.get("PASSWORD")
+            dbname = settings_dict["NAME"]
+            user = settings_dict["USER"]
+            passwd = settings_dict["PASSWORD"]
             auth_database = options.get("authenticationDatabase")
             auth_mechanism = options.get("authenticationMechanism")
             retry_writes = options.get("retryWrites")
@@ -34,13 +35,19 @@ class DatabaseClient(BaseDatabaseClient):
             if auth_mechanism:
                 args += ["--authenticationMechanism", auth_mechanism]
             if retry_writes is not None:
-                args += ["--retryWrites", str(bool(retry_writes)).lower()]
+                args += ["--retryWrites", str(retry_writes).lower()]
             if host:
                 uri = f"{protocol}://{host}/{dbname}"
+            elif dbname:
+                uri = dbname
 
         if uri:
             args.append(uri)
-        args += options.get("filenames", []) + parameters
+
+        if parameters:
+            if not any(param in cls.help_options for param in parameters):
+                args.append("--shell")
+            args += parameters
         return args, None
 
     def runshell(self, parameters):
