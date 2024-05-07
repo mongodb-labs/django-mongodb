05678f6d4670c0847b5330b42b4fb3ad7d5a4e8a
Emanuel Lupi <emanuel.lupi91@gmail.com>
2024-05-07 18:02:16 -0300

    Rename mongosh parameters.

--- django_mongodb/client.py
+++ django_mongodb/client.py
@@ -1,4 +1,5 @@
 import signal
+import urllib
 
 from django.db.backends.base.client import BaseDatabaseClient
 
@@ -10,39 +11,35 @@ class DatabaseClient(BaseDatabaseClient):
     @classmethod
     def settings_to_cmd_args_env(cls, settings_dict, parameters):
         options = settings_dict.get("OPTIONS", {})
-        uri = options.get("uri")
         args = [cls.executable_name]
-        if not uri:
-            host = settings_dict["HOST"]
-            port = settings_dict["PORT"]
-            if port and host:
-                host += f":{port}"
 
-            dbname = settings_dict["NAME"]
-            user = settings_dict["USER"]
-            passwd = settings_dict["PASSWORD"]
-            auth_database = options.get("authenticationDatabase")
-            auth_mechanism = options.get("authenticationMechanism")
-            retry_writes = options.get("retryWrites")
-            protocol = options.get("protocol", "mongodb")
+        host = settings_dict["HOST"]
+        port = settings_dict["PORT"]
+        dbname = settings_dict["NAME"] or "test"
+        user = settings_dict["USER"]
+        passwd = settings_dict["PASSWORD"]
+        auth_database = options.get("authSource")
+        auth_mechanism = options.get("authMechanism")
+        retry_writes = options.get("retryWrites")
 
-            if user:
-                args += ["--username", user]
-            if passwd:
-                args += ["--password", passwd]
-            if auth_database:
-                args += ["--authenticationDatabase", auth_database]
-            if auth_mechanism:
-                args += ["--authenticationMechanism", auth_mechanism]
-            if retry_writes is not None:
-                args += ["--retryWrites", str(retry_writes).lower()]
-            if host:
-                uri = f"{protocol}://{host}/{dbname}"
-            elif dbname:
-                uri = dbname
+        if host:
+            args += ["--host", host]
+        if port:
+            args += ["--port", port]
+        if user:
+            args += ["--username", user]
+        if passwd:
+            passwd = urllib.parse.quote_plus(passwd)
+            args += ["--password", passwd]
+        if auth_database:
+            args += ["--authenticationDatabase", auth_database]
+        if auth_mechanism:
+            args += ["--authenticationMechanism", auth_mechanism]
+        if retry_writes is not None:
+            args += ["--retryWrites", str(retry_writes).lower()]
 
-        if uri:
-            args.append(uri)
+        if dbname:
+            args.append(dbname)
 
         if parameters:
             if not any(param in cls.help_options for param in parameters):
