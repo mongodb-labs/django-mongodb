a06c1eb73c170e83ee94a8233abd5db7634deb68
Emanuel Lupi <emanuel.lupi91@gmail.com>
2024-05-08 00:07:16 -0300

    MongoDb shell.

--- django_mongodb/client.py
+++ django_mongodb/client.py
@@ -4,11 +4,43 @@ from django.db.backends.base.client import BaseDatabaseClient
 
 
 class DatabaseClient(BaseDatabaseClient):
-    executable_name = "mongo"
+    executable_name = "mongosh"
 
     @classmethod
     def settings_to_cmd_args_env(cls, settings_dict, parameters):
-        raise NotImplementedError
+        options = settings_dict.get("OPTIONS", {})
+        args = [cls.executable_name]
+
+        host = settings_dict["HOST"]
+        port = settings_dict["PORT"]
+        dbname = settings_dict["NAME"] or "test"
+        user = settings_dict["USER"]
+        passwd = settings_dict["PASSWORD"]
+        auth_database = options.get("authSource")
+        auth_mechanism = options.get("authMechanism")
+        retry_writes = options.get("retryWrites")
+
+        if host:
+            args += ["--host", host]
+        if port:
+            args += ["--port", port]
+        if user:
+            args += ["--username", user]
+        if passwd:
+            args += ["--password", passwd]
+        if auth_database:
+            args += ["--authenticationDatabase", auth_database]
+        if auth_mechanism:
+            args += ["--authenticationMechanism", auth_mechanism]
+        if retry_writes is not None:
+            args += ["--retryWrites", str(retry_writes).lower()]
+
+        args.append(dbname)
+
+        if parameters:
+            args.append("--shell")
+            args += parameters
+        return args, None
 
     def runshell(self, parameters):
         sigint_handler = signal.getsignal(signal.SIGINT)
