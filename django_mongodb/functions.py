fa3ef6e423b214663238c89c426dafd01f6a30c5
Tim Graham <timograham@gmail.com>
2024-06-04 11:30:27 -0400

    add support for math database functions

--- django_mongodb/functions.py
+++ django_mongodb/functions.py
@@ -1,7 +1,24 @@
 from django.db import NotSupportedError
+from django.db.models.expressions import Func
 from django.db.models.functions.datetime import Extract
+from django.db.models.functions.math import Ceil, Cot, Degrees, Log, Power, Radians, Random, Round
+from django.db.models.functions.text import Upper
 
 from .query_utils import process_lhs
+
+MONGO_OPERATORS = {
+    Ceil: "ceil",
+    Degrees: "radiansToDegrees",
+    Power: "pow",
+    Radians: "degreesToRadians",
+    Random: "rand",
+    Upper: "toUpper",
+}
+
+
+def cot(self, compiler, connection):
+    lhs_mql = process_lhs(self, compiler, connection)
+    return {"$divide": [1, {"$tan": lhs_mql}]}
 
 
 def extract(self, compiler, connection):
@@ -17,5 +34,28 @@ def extract(self, compiler, connection):
     return {operator: lhs_mql}
 
 
+def func(self, compiler, connection):
+    lhs_mql = process_lhs(self, compiler, connection)
+    operator = MONGO_OPERATORS.get(self.__class__, self.function.lower())
+    return {f"${operator}": lhs_mql}
+
+
+def log(self, compiler, connection):
+    # This function is usually log(base, num) but on MongoDB it's log(num, base).
+    clone = self.copy()
+    clone.set_source_expressions(self.get_source_expressions()[::-1])
+    return func(clone, compiler, connection)
+
+
+def round_(self, compiler, connection):
+    # Round needs its own function because it's a special case that inherits
+    # from Transform but has two arguments.
+    return {"$round": [expr.as_mql(compiler, connection) for expr in self.get_source_expressions()]}
+
+
 def register_functions():
+    Cot.as_mql_agg = cot
     Extract.as_mql = extract
+    Func.as_mql_agg = func
+    Log.as_mql_agg = log
+    Round.as_mql_agg = round_
