21ed108a18de4f1c4b5f0c8480ca28aa88c3dd84
Tim Graham <timograham@gmail.com>
2024-06-13 19:58:44 -0400

    add support for Case/When expressions

--- django_mongodb/expressions.py
+++ django_mongodb/expressions.py
@@ -1,10 +1,38 @@
+from django.core.exceptions import EmptyResultSet, FullResultSet
 from django.db.models.expressions import (
+    Case,
     Col,
     CombinedExpression,
     ExpressionWrapper,
     NegatedExpression,
     Value,
+    When,
 )
+
+
+def case(self, compiler, connection):
+    case_parts = []
+    for case in self.cases:
+        case_mql = {}
+        try:
+            case_mql["case"] = case.as_mql(compiler, connection)
+        except EmptyResultSet:
+            continue
+        except FullResultSet:
+            default_mql = case.result.as_mql(compiler, connection)
+            break
+        case_mql["then"] = case.result.as_mql(compiler, connection)
+        case_parts.append(case_mql)
+    else:
+        default_mql = self.default.as_mql(compiler, connection)
+    if not case_parts:
+        return default_mql
+    return {
+        "$switch": {
+            "branches": case_parts,
+            "default": default_mql,
+        }
+    }
 
 
 def col(self, compiler, connection):  # noqa: ARG001
@@ -27,13 +55,19 @@ def negated_expression(self, compiler, connection):
     return {"$not": expression_wrapper(self, compiler, connection)}
 
 
+def when(self, compiler, connection):
+    return self.condition.as_mql(compiler, connection)
+
+
 def value(self, compiler, connection):  # noqa: ARG001
     return {"$literal": self.value}
 
 
 def register_expressions():
+    Case.as_mql = case
     Col.as_mql = col
     CombinedExpression.as_mql = combined_expression
     ExpressionWrapper.as_mql = expression_wrapper
     NegatedExpression.as_mql = negated_expression
+    When.as_mql = when
     Value.as_mql = value
