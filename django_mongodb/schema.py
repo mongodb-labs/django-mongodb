d99ddef6ef2178ffe628f1fa663bf3ec98641f1e
Tim Graham <timograham@gmail.com>
2024-08-23 20:30:14 -0400

    stub out SchemaEditor methods

--- django_mongodb/schema.py
+++ django_mongodb/schema.py
@@ -5,6 +5,9 @@ class DatabaseSchemaEditor(BaseDatabaseSchemaEditor):
     def create_model(self, model):
         pass
 
+    def delete_model(self, model):
+        pass
+
     def add_field(self, model, field):
         pass
 
@@ -14,11 +17,26 @@ class DatabaseSchemaEditor(BaseDatabaseSchemaEditor):
     def remove_field(self, model, field):
         pass
 
+    def alter_index_together(self, model, old_index_together, new_index_together):
+        pass
+
     def alter_unique_together(self, model, old_unique_together, new_unique_together):
         pass
 
     def add_index(self, model, index):
         pass
 
+    def rename_index(self, model, old_index, new_index):
+        pass
+
     def remove_index(self, model, index):
+        pass
+
+    def add_constraint(self, model, constraint):
+        pass
+
+    def remove_constraint(self, model, constraint):
+        pass
+
+    def alter_db_table(self, model, old_db_table, new_db_table):
         pass
