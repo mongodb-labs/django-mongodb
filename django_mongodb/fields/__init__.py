0ca00d5e777b59b11ebee7f519878697af5f2038
Tim Graham <timograham@gmail.com>
2024-09-20 19:22:57 -0400

    add support for EmbeddedModelField

--- django_mongodb/fields/__init__.py
+++ django_mongodb/fields/__init__.py
@@ -1,9 +1,10 @@
 from .auto import ObjectIdAutoField
 from .duration import register_duration_field
+from .embedded_model import EmbeddedModelField
 from .json import register_json_field
 from .objectid import ObjectIdField
 
-__all__ = ["register_fields", "ObjectIdAutoField", "ObjectIdField"]
+__all__ = ["register_fields", "EmbeddedModelField", "ObjectIdAutoField", "ObjectIdField"]
 
 
 def register_fields():
--- tests/model_fields_/models.py
+++ tests/model_fields_/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
-from django_mongodb.fields import ObjectIdField
+from django_mongodb.fields import EmbeddedModelField, ObjectIdField
 
 
+# ObjectIdField
 class ObjectIdModel(models.Model):
     field = ObjectIdField()
 
@@ -13,3 +14,33 @@ class NullableObjectIdModel(models.Model):
 
 class PrimaryKeyObjectIdModel(models.Model):
     field = ObjectIdField(primary_key=True)
+
+
+# EmbeddedModelField
+class Target(models.Model):
+    index = models.IntegerField()
+
+
+class DecimalModel(models.Model):
+    decimal = models.DecimalField(max_digits=9, decimal_places=2)
+
+
+class DecimalKey(models.Model):
+    decimal = models.DecimalField(max_digits=9, decimal_places=2, primary_key=True)
+
+
+class DecimalParent(models.Model):
+    child = models.ForeignKey(DecimalKey, models.CASCADE)
+
+
+class EmbeddedModelFieldModel(models.Model):
+    simple = EmbeddedModelField("EmbeddedModel", null=True, blank=True)
+    untyped = EmbeddedModelField(null=True, blank=True)
+    decimal_parent = EmbeddedModelField(DecimalParent, null=True, blank=True)
+
+
+class EmbeddedModel(models.Model):
+    some_relation = models.ForeignKey(Target, models.CASCADE, null=True, blank=True)
+    someint = models.IntegerField(db_column="custom_column")
+    auto_now = models.DateTimeField(auto_now=True)
+    auto_now_add = models.DateTimeField(auto_now_add=True)
