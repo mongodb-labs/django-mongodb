256fbff6037a5f57da567f11c6b2ba0be655a336
Steven Silvester <steve.silvester@mongodb.com>
2024-10-02 10:19:12 -0500

    handle warnings and version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,17 +7,19 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 from __future__ import annotations
 
+from importlib.metadata import version as _version
+
 project = "django_mongodb"
 copyright = "2024, The MongoDB Python Team"
 author = "The MongoDB Python Team"
-release = "0.1.0.dev0"
+release = _version("django_mongodb")
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ["_templates"]
+# templates_path = ["_templates"]
 exclude_patterns = []
 
 
@@ -25,4 +27,4 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "alabaster"
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
