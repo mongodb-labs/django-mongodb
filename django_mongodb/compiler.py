mongodb-labs/django-mongodb

commit e9711203ac720f34ce14ecb53489828887c4ef64

add support for subqueries
Subquery, Exists, and QuerySet as a lookup value.

django_mongodb/compiler.py
@@ -13,6 +13,7 @@ from django.db.models.functions.math import Power
 from django.db.models.lookups import IsNull
 from django.db.models.sql import compiler
 from django.db.models.sql.constants import GET_ITERATOR_CHUNK_SIZE, MULTI, SINGLE
+from django.db.models.sql.datastructures import BaseTable
 from django.utils.functional import cached_property
 from pymongo import ASCENDING, DESCENDING
 
@@ -25,12 +26,16 @@ class SQLCompiler(compiler.SQLCompiler):
 
     query_class = MongoQuery
     GROUP_SEPARATOR = "___"
+    PARENT_FIELD_TEMPLATE = "parent__field__{}"
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.aggregation_pipeline = None
+        # Map columns to their subquery indices.
+        self.column_indices = {}
         # A list of OrderBy objects for this query.
         self.order_by_objs = None
+        self.subqueries = []
 
     def _unfold_column(self, col):
         """
@@ -154,23 +159,40 @@ class SQLCompiler(compiler.SQLCompiler):
         group.update(having_group)
         return group, replacements
 
-    def _get_group_id_expressions(self, order_by):
-        """Generate group ID expressions for the aggregation pipeline."""
-        group_expressions = set()
-        replacements = {}
+    def _get_group_expressions(self, order_by):
+        if self.query.group_by is None:
+            return []
+        seen = set()
+        expressions = set()
+        if self.query.group_by is not True:
+            # If group_by isn't True, then it's a list of expressions.
+            for expr in self.query.group_by:
+                if not hasattr(expr, "as_sql"):
+                    expr = self.query.resolve_ref(expr)
+                if isinstance(expr, Ref):
+                    if expr.refs not in seen:
+                        seen.add(expr.refs)
+                        expressions.add(expr.source)
+                else:
+                    expressions.add(expr)
+        for expr, _, alias in self.select:
+            # Skip members that are already grouped.
+            if alias not in seen:
+                expressions |= set(expr.get_group_by_cols())
         if not self._meta_ordering:
             for expr, (_, _, is_ref) in order_by:
+                # Skip references.
                 if not is_ref:
-                    group_expressions |= set(expr.get_group_by_cols())
-        for expr, *_ in self.select:
-            group_expressions |= set(expr.get_group_by_cols())
+                    expressions |= set(expr.get_group_by_cols())
         having_group_by = self.having.get_group_by_cols() if self.having else ()
         for expr in having_group_by:
-            group_expressions.add(expr)
-        if isinstance(self.query.group_by, tuple | list):
-            group_expressions |= set(self.query.group_by)
-        elif self.query.group_by is None:
-            group_expressions = set()
+            expressions.add(expr)
+        return expressions
+
+    def _get_group_id_expressions(self, order_by):
+        """Generate group ID expressions for the aggregation pipeline."""
+        replacements = {}
+        group_expressions = self._get_group_expressions(order_by)
         if not group_expressions:
             ids = None
         else:
@@ -186,6 +208,8 @@ class SQLCompiler(compiler.SQLCompiler):
                     ids[alias] = Value(True).as_mql(self, self.connection)
                 if replacement is not None:
                     replacements[col] = replacement
+                    if isinstance(col, Ref):
+                        replacements[col.source] = replacement
         return ids, replacements
 
     def _build_aggregation_pipeline(self, ids, group):
@@ -228,15 +252,15 @@ class SQLCompiler(compiler.SQLCompiler):
             all_replacements.update(replacements)
             pipeline = self._build_aggregation_pipeline(ids, group)
             if self.having:
-                pipeline.append(
-                    {
-                        "$match": {
-                            "$expr": self.having.replace_expressions(all_replacements).as_mql(
-                                self, self.connection
-                            )
-                        }
-                    }
+                having = self.having.replace_expressions(all_replacements).as_mql(
+                    self, self.connection
                 )
+                # Add HAVING subqueries.
+                for query in self.subqueries or ():
+                    pipeline.extend(query.get_pipeline())
+                # Remove the added subqueries.
+                self.subqueries = []
+                pipeline.append({"$match": {"$expr": having}})
             self.aggregation_pipeline = pipeline
         self.annotations = {
             target: expr.replace_expressions(all_replacements)
@@ -388,6 +412,7 @@ class SQLCompiler(compiler.SQLCompiler):
                 query.mongo_query = {"$expr": expr}
         if extra_fields:
             query.extra_fields = self.get_project_fields(extra_fields, force_expression=True)
+        query.subqueries = self.subqueries
         return query
 
     def get_columns(self):
@@ -431,7 +456,12 @@ class SQLCompiler(compiler.SQLCompiler):
 
     @cached_property
     def collection_name(self):
-        return self.query.get_meta().db_table
+        base_table = next(
+            v
+            for k, v in self.query.alias_map.items()
+            if isinstance(v, BaseTable) and self.query.alias_refcount[k]
+        )
+        return base_table.table_alias or base_table.table_name
 
     @cached_property
     def collection(self):
@@ -581,7 +611,7 @@ class SQLCompiler(compiler.SQLCompiler):
         return tuple(fields), sort_ordering, tuple(extra_fields)
 
     def get_where(self):
-        return self.where
+        return getattr(self, "where", self.query.where)
 
     def explain_query(self):
         # Validate format (none supported) and options.
@@ -741,7 +771,7 @@ class SQLAggregateCompiler(SQLCompiler):
             else None
         )
         subquery = compiler.build_query(columns)
-        query.subquery = subquery
+        query.subqueries = [subquery]
         return query
 
     def _make_result(self, result, columns=None):

django_mongodb/features.py
@@ -10,6 +10,7 @@ class DatabaseFeatures(BaseDatabaseFeatures):
     greatest_least_ignores_nulls = True
     has_json_object_function = False
     has_native_json_field = True
+    supports_boolean_expr_in_select_clause = True
     supports_collation_on_charfield = False
     supports_column_check_constraints = False
     supports_date_lookup_using_string = False
@@ -91,6 +92,19 @@ class DatabaseFeatures(BaseDatabaseFeatures):
         "schema.tests.SchemaTests.test_composed_constraint_with_fk",
         "schema.tests.SchemaTests.test_remove_ignored_unique_constraint_not_create_fk_index",
         "schema.tests.SchemaTests.test_unique_constraint",
+        # Column default values aren't handled when a field raises
+        # EmptyResultSet: https://github.com/mongodb-labs/django-mongodb/issues/155
+        "annotations.tests.NonAggregateAnnotationTestCase.test_empty_queryset_annotation",
+        "db_functions.comparison.test_coalesce.CoalesceTests.test_empty_queryset",
+        # Union as subquery is not mapping the parent parameter and collections:
+        # https://github.com/mongodb-labs/django-mongodb/issues/156
+        "queries.test_qs_combinators.QuerySetSetOperationTests.test_union_in_subquery_related_outerref",
+        "queries.test_qs_combinators.QuerySetSetOperationTests.test_union_in_subquery",
+        "queries.test_qs_combinators.QuerySetSetOperationTests.test_union_in_with_ordering",
+        # ObjectId type mismatch in a subquery:
+        # https://github.com/mongodb-labs/django-mongodb/issues/161
+        "queries.tests.RelatedLookupTypeTests.test_values_queryset_lookup",
+        "queries.tests.ValuesSubqueryTests.test_values_in_subquery",
     }
     # $bitAnd, #bitOr, and $bitXor are new in MongoDB 6.3.
     _django_test_expected_failures_bitwise = {
@@ -204,166 +218,9 @@ class DatabaseFeatures(BaseDatabaseFeatures):
         },
         "Test assumes integer primary key.": {
             "db_functions.comparison.test_cast.CastTests.test_cast_to_integer_foreign_key",
+            "expressions.tests.BasicExpressionsTests.test_nested_subquery_outer_ref_with_autofield",
             "model_fields.test_foreignkey.ForeignKeyTests.test_to_python",
             "queries.test_qs_combinators.QuerySetSetOperationTests.test_order_raises_on_non_selected_column",
-        },
-        "Exists is not supported on MongoDB.": {
-            "aggregation.test_filter_argument.FilteredAggregateTests.test_filtered_aggregate_on_exists",
-            "aggregation.test_filter_argument.FilteredAggregateTests.test_filtered_aggregate_ref_multiple_subquery_annotation",
-            "aggregation.tests.AggregateTestCase.test_aggregation_exists_multivalued_outeref",
-            "aggregation.tests.AggregateTestCase.test_group_by_exists_annotation",
-            "aggregation.tests.AggregateTestCase.test_exists_none_with_aggregate",
-            "aggregation.tests.AggregateTestCase.test_exists_extra_where_with_aggregate",
-            "annotations.tests.NonAggregateAnnotationTestCase.test_annotation_exists_aggregate_values_chaining",
-            "annotations.tests.NonAggregateAnnotationTestCase.test_annotation_exists_none_query",
-            "aggregation_regress.tests.AggregationTests.test_annotate_and_join",
-            "delete_regress.tests.DeleteTests.test_self_reference_with_through_m2m_at_second_level",
-            "expressions.tests.BasicExpressionsTests.test_annotation_with_deeply_nested_outerref",
-            "expressions.tests.BasicExpressionsTests.test_boolean_expression_combined",
-            "expressions.tests.BasicExpressionsTests.test_boolean_expression_combined_with_empty_Q",
-            "expressions.tests.BasicExpressionsTests.test_boolean_expression_in_Q",
-            "expressions.tests.BasicExpressionsTests.test_case_in_filter_if_boolean_output_field",
-            "expressions.tests.BasicExpressionsTests.test_exists_in_filter",
-            "expressions.tests.BasicExpressionsTests.test_order_by_exists",
-            "expressions.tests.BasicExpressionsTests.test_subquery",
-            "expressions.tests.ExistsTests.test_filter_by_empty_exists",
-            "expressions.tests.ExistsTests.test_negated_empty_exists",
-            "expressions.tests.ExistsTests.test_optimizations",
-            "expressions.tests.ExistsTests.test_select_negated_empty_exists",
-            "lookup.tests.LookupTests.test_exact_exists",
-            "lookup.tests.LookupTests.test_nested_outerref_lhs",
-            "lookup.tests.LookupQueryingTests.test_filter_exists_lhs",
-            "model_forms.tests.LimitChoicesToTests.test_fields_for_model_applies_limit_choices_to",
-            "model_forms.tests.LimitChoicesToTests.test_limit_choices_to_callable_for_fk_rel",
-            "model_forms.tests.LimitChoicesToTests.test_limit_choices_to_callable_for_m2m_rel",
-            "model_forms.tests.LimitChoicesToTests.test_limit_choices_to_m2m_through",
-            "model_forms.tests.LimitChoicesToTests.test_limit_choices_to_no_duplicates",
-            "null_queries.tests.NullQueriesTests.test_reverse_relations",
-            "queries.test_qs_combinators.QuerySetSetOperationTests.test_union_with_values_list_on_annotated_and_unannotated",
-            "queries.tests.ExcludeTest17600.test_exclude_plain",
-            "queries.tests.ExcludeTest17600.test_exclude_with_q_is_equal_to_plain_exclude_variation",
-            "queries.tests.ExcludeTest17600.test_exclude_with_q_object_no_distinct",
-            "queries.tests.ExcludeTests.test_exclude_multivalued_exists",
-            "queries.tests.ExcludeTests.test_exclude_reverse_fk_field_ref",
-            "queries.tests.ExcludeTests.test_exclude_with_circular_fk_relation",
-            "queries.tests.ExcludeTests.test_subquery_exclude_outerref",
-            "queries.tests.ExcludeTests.test_to_field",
-            "queries.tests.ForeignKeyToBaseExcludeTests.test_ticket_21787",
-            "queries.tests.JoinReuseTest.test_inverted_q_across_relations",
-            "queries.tests.ManyToManyExcludeTest.test_exclude_many_to_many",
-            "queries.tests.ManyToManyExcludeTest.test_ticket_12823",
-            "queries.tests.Queries1Tests.test_double_exclude",
-            "queries.tests.Queries1Tests.test_exclude",
-            "queries.tests.Queries1Tests.test_exclude_in",
-            "queries.tests.Queries1Tests.test_excluded_intermediary_m2m_table_joined",
-            "queries.tests.Queries1Tests.test_nested_exclude",
-            "queries.tests.Queries4Tests.test_join_reuse_order",
-            "queries.tests.Queries4Tests.test_ticket24525",
-            "queries.tests.Queries6Tests.test_tickets_8921_9188",
-            "queries.tests.Queries6Tests.test_xor_subquery",
-            "queries.tests.QuerySetBitwiseOperationTests.test_subquery_aliases",
-            "queries.tests.TestTicket24605.test_ticket_24605",
-            "queries.tests.Ticket20101Tests.test_ticket_20101",
-            "queries.tests.Ticket20788Tests.test_ticket_20788",
-            "queries.tests.Ticket22429Tests.test_ticket_22429",
-        },
-        "Subquery is not supported on MongoDB.": {
-            "aggregation.test_filter_argument.FilteredAggregateTests.test_filtered_aggregate_ref_subquery_annotation",
-            "aggregation.tests.AggregateAnnotationPruningTests.test_referenced_composed_subquery_requires_wrapping",
-            "aggregation.tests.AggregateAnnotationPruningTests.test_referenced_subquery_requires_wrapping",
-            "aggregation.tests.AggregateTestCase.test_aggregation_nested_subquery_outerref",
-            "aggregation.tests.AggregateTestCase.test_aggregation_subquery_annotation",
-            "aggregation.tests.AggregateTestCase.test_aggregation_subquery_annotation_multivalued",
-            "aggregation.tests.AggregateTestCase.test_aggregation_subquery_annotation_related_field",
-            "aggregation.tests.AggregateTestCase.test_aggregation_subquery_annotation_values",
-            "aggregation.tests.AggregateTestCase.test_aggregation_subquery_annotation_values_collision",
-            "annotations.tests.NonAggregateAnnotationTestCase.test_annotation_filter_with_subquery",
-            "annotations.tests.NonAggregateAnnotationTestCase.test_annotation_subquery_and_aggregate_values_chaining",
-            "annotations.tests.NonAggregateAnnotationTestCase.test_annotation_subquery_outerref_transform",
-            "annotations.tests.NonAggregateAnnotationTestCase.test_empty_queryset_annotation",
-            "db_functions.datetime.test_extract_trunc.DateFunctionTests.test_extract_outerref",
-            "db_functions.datetime.test_extract_trunc.DateFunctionTests.test_trunc_subquery_with_parameters",
-            "expressions.tests.BasicExpressionsTests.test_aggregate_subquery_annotation",
-            "expressions.tests.BasicExpressionsTests.test_annotation_with_nested_outerref",
-            "expressions.tests.BasicExpressionsTests.test_annotation_with_outerref",
-            "expressions.tests.BasicExpressionsTests.test_annotations_within_subquery",
-            "expressions.tests.BasicExpressionsTests.test_in_subquery",
-            "expressions.tests.BasicExpressionsTests.test_nested_outerref_with_function",
-            "expressions.tests.BasicExpressionsTests.test_nested_subquery",
-            "expressions.tests.BasicExpressionsTests.test_nested_subquery_join_outer_ref",
-            "expressions.tests.BasicExpressionsTests.test_nested_subquery_outer_ref_2",
-            "expressions.tests.BasicExpressionsTests.test_nested_subquery_outer_ref_with_autofield",
-            "expressions.tests.BasicExpressionsTests.test_outerref_mixed_case_table_name",
-            "expressions.tests.BasicExpressionsTests.test_outerref_with_operator",
-            "expressions.tests.BasicExpressionsTests.test_subquery_filter_by_aggregate",
-            "expressions.tests.BasicExpressionsTests.test_subquery_filter_by_lazy",
-            "expressions.tests.BasicExpressionsTests.test_subquery_group_by_outerref_in_filter",
-            "expressions.tests.BasicExpressionsTests.test_subquery_in_filter",
-            "expressions.tests.BasicExpressionsTests.test_subquery_references_joined_table_twice",
-            "expressions.tests.BasicExpressionsTests.test_uuid_pk_subquery",
-            "lookup.tests.LookupQueryingTests.test_filter_subquery_lhs",
-            "model_fields.test_jsonfield.TestQuerying.test_nested_key_transform_on_subquery",
-            "model_fields.test_jsonfield.TestQuerying.test_obj_subquery_lookup",
-            "queries.test_qs_combinators.QuerySetSetOperationTests.test_union_in_subquery",
-            "queries.test_qs_combinators.QuerySetSetOperationTests.test_union_in_subquery_related_outerref",
-        },
-        "Using a QuerySet in annotate() is not supported on MongoDB.": {
-            "aggregation.test_filter_argument.FilteredAggregateTests.test_filtered_reused_subquery",
-            "aggregation.tests.AggregateTestCase.test_filter_in_subquery_or_aggregation",
-            "aggregation.tests.AggregateTestCase.test_group_by_subquery_annotation",
-            "aggregation.tests.AggregateTestCase.test_group_by_reference_subquery",
-            "annotations.tests.NonAggregateAnnotationTestCase.test_annotation_and_alias_filter_in_subquery",
-            "annotations.tests.NonAggregateAnnotationTestCase.test_annotation_and_alias_filter_related_in_subquery",
-            "annotations.tests.NonAggregateAnnotationTestCase.test_empty_expression_annotation",
-            "aggregation_regress.tests.AggregationTests.test_aggregates_in_where_clause",
-            "aggregation_regress.tests.AggregationTests.test_aggregates_in_where_clause_pre_eval",
-            "aggregation_regress.tests.AggregationTests.test_f_expression_annotation",
-            "aggregation_regress.tests.AggregationTests.test_having_subquery_select",
-            "aggregation_regress.tests.AggregationTests.test_more_more4",
-            "aggregation_regress.tests.AggregationTests.test_more_more_more5",
-            "aggregation_regress.tests.AggregationTests.test_negated_aggregation",
-            "db_functions.comparison.test_coalesce.CoalesceTests.test_empty_queryset",
-            "expressions.tests.FTimeDeltaTests.test_date_subquery_subtraction",
-            "expressions.tests.FTimeDeltaTests.test_datetime_subquery_subtraction",
-            "expressions.tests.FTimeDeltaTests.test_time_subquery_subtraction",
-            "expressions_case.tests.CaseExpressionTests.test_annotate_with_in_clause",
-            "expressions_case.tests.CaseExpressionTests.test_in_subquery",
-            "lookup.tests.LookupTests.test_exact_query_rhs_with_selected_columns",
-            "lookup.tests.LookupTests.test_exact_sliced_queryset_limit_one",
-            "lookup.tests.LookupTests.test_exact_sliced_queryset_limit_one_offset",
-            "lookup.tests.LookupTests.test_in_different_database",
-            "many_to_many.tests.ManyToManyTests.test_assign",
-            "many_to_many.tests.ManyToManyTests.test_assign_ids",
-            "many_to_many.tests.ManyToManyTests.test_clear",
-            "many_to_many.tests.ManyToManyTests.test_remove",
-            "many_to_many.tests.ManyToManyTests.test_reverse_assign_with_queryset",
-            "many_to_many.tests.ManyToManyTests.test_set",
-            "many_to_many.tests.ManyToManyTests.test_set_existing_different_type",
-            "many_to_one.tests.ManyToOneTests.test_get_prefetch_queryset_reverse_warning",
-            "model_fields.test_jsonfield.TestQuerying.test_usage_in_subquery",
-            "one_to_one.tests.OneToOneTests.test_get_prefetch_queryset_warning",
-            "one_to_one.tests.OneToOneTests.test_rel_pk_subquery",
-            "queries.test_qs_combinators.QuerySetSetOperationTests.test_union_in_with_ordering",
-            "queries.tests.CloneTests.test_evaluated_queryset_as_argument",
-            "queries.tests.DoubleInSubqueryTests.test_double_subquery_in",
-            "queries.tests.EmptyQuerySetTests.test_values_subquery",
-            "queries.tests.ExcludeTests.test_exclude_subquery",
-            "queries.tests.NullInExcludeTest.test_null_in_exclude_qs",
-            "queries.tests.Queries1Tests.test_ticket9985",
-            "queries.tests.Queries1Tests.test_ticket9997",
-            "queries.tests.Queries1Tests.test_ticket10742",
-            "queries.tests.Queries4Tests.test_ticket10181",
-            "queries.tests.Queries5Tests.test_queryset_reuse",
-            "queries.tests.QuerySetBitwiseOperationTests.test_conflicting_aliases_during_combine",
-            "queries.tests.RelabelCloneTest.test_ticket_19964",
-            "queries.tests.RelatedLookupTypeTests.test_correct_lookup",
-            "queries.tests.RelatedLookupTypeTests.test_values_queryset_lookup",
-            "queries.tests.Ticket23605Tests.test_ticket_23605",
-            "queries.tests.ToFieldTests.test_in_subquery",
-            "queries.tests.ToFieldTests.test_nested_in_subquery",
-            "queries.tests.ValuesSubqueryTests.test_values_in_subquery",
-            "queries.tests.WeirdQuerysetSlicingTests.test_empty_sliced_subquery",
-            "queries.tests.WeirdQuerysetSlicingTests.test_empty_sliced_subquery_exclude",
         },
         "Cannot use QuerySet.delete() when querying across multiple collections on MongoDB.": {
             "delete.tests.FastDeleteTests.test_fast_delete_aggregation",
@@ -375,6 +232,16 @@ class DatabaseFeatures(BaseDatabaseFeatures):
             "delete_regress.tests.Ticket19102Tests.test_ticket_19102_defer",
             "delete_regress.tests.Ticket19102Tests.test_ticket_19102_select_related",
             "one_to_one.tests.OneToOneTests.test_o2o_primary_key_delete",
+        },
+        "Cannot use QuerySet.delete() when a subquery is required.": {
+            "delete_regress.tests.DeleteTests.test_self_reference_with_through_m2m_at_second_level",
+            "many_to_many.tests.ManyToManyTests.test_assign",
+            "many_to_many.tests.ManyToManyTests.test_assign_ids",
+            "many_to_many.tests.ManyToManyTests.test_clear",
+            "many_to_many.tests.ManyToManyTests.test_remove",
+            "many_to_many.tests.ManyToManyTests.test_reverse_assign_with_queryset",
+            "many_to_many.tests.ManyToManyTests.test_set",
+            "many_to_many.tests.ManyToManyTests.test_set_existing_different_type",
         },
         "Cannot use QuerySet.update() when querying across multiple collections on MongoDB.": {
             "expressions.tests.BasicExpressionsTests.test_filter_with_join",
@@ -417,6 +284,7 @@ class DatabaseFeatures(BaseDatabaseFeatures):
             "ordering.tests.OrderingTests.test_orders_nulls_first_on_filtered_subquery",
             "queries.tests.ExcludeTest17600.test_exclude_plain_distinct",
             "queries.tests.ExcludeTest17600.test_exclude_with_q_is_equal_to_plain_exclude",
+            "queries.tests.ExcludeTest17600.test_exclude_with_q_is_equal_to_plain_exclude_variation",
             "queries.tests.ExcludeTest17600.test_exclude_with_q_object_distinct",
             "queries.tests.ExcludeTests.test_exclude_m2m_through",
             "queries.tests.ExistsSql.test_distinct_exists",
@@ -433,6 +301,7 @@ class DatabaseFeatures(BaseDatabaseFeatures):
             "update.tests.AdvancedTests.test_update_all",
         },
         "QuerySet.extra() is not supported.": {
+            "aggregation.tests.AggregateTestCase.test_exists_extra_where_with_aggregate",
             "annotations.tests.NonAggregateAnnotationTestCase.test_column_field_ordering",
             "annotations.tests.NonAggregateAnnotationTestCase.test_column_field_ordering_with_deferred",
             "basic.tests.ModelTest.test_extra_method_select_argument_with_dashes",
@@ -468,6 +337,7 @@ class DatabaseFeatures(BaseDatabaseFeatures):
             "aggregation.tests.AggregateAnnotationPruningTests.test_referenced_aggregate_annotation_kept",
             "aggregation.tests.AggregateTestCase.test_count_star",
             "delete.tests.DeletionTests.test_only_referenced_fields_selected",
+            "expressions.tests.ExistsTests.test_optimizations",
             "lookup.tests.LookupTests.test_in_ignore_none",
             "lookup.tests.LookupTests.test_textfield_exact_null",
             "migrations.test_commands.MigrateTests.test_migrate_syncdb_app_label",

commit b3fd2c479c653bbe4fe3f1e6c4057ce5bf93711f

fix SQLCompiler.collection_name crash on QuerySet.update() with MTI
Regression in e9711203ac720f34ce14ecb53489828887c4ef64.

django_mongodb/compiler.py
@@ -673,6 +673,10 @@ class SQLInsertCompiler(SQLCompiler):
         inserted_ids = self.collection.insert_many(docs).inserted_ids
         return inserted_ids if returning_fields else []
 
+    @cached_property
+    def collection_name(self):
+        return self.query.get_meta().db_table
+
 
 class SQLDeleteCompiler(compiler.SQLDeleteCompiler, SQLCompiler):
     def execute_sql(self, result_type=MULTI):
@@ -689,6 +693,10 @@ class SQLDeleteCompiler(compiler.SQLDeleteCompiler, SQLCompiler):
 
     def get_where(self):
         return self.query.where
+
+    @cached_property
+    def collection_name(self):
+        return self.query.base_table
 
 
 class SQLUpdateCompiler(compiler.SQLUpdateCompiler, SQLCompiler):
@@ -753,6 +761,10 @@ class SQLUpdateCompiler(compiler.SQLUpdateCompiler, SQLCompiler):
 
     def get_where(self):
         return self.query.where
+
+    @cached_property
+    def collection_name(self):
+        return self.query.base_table
 
 
 class SQLAggregateCompiler(SQLCompiler):

django_mongodb/features.py
@@ -105,9 +105,6 @@ class DatabaseFeatures(BaseDatabaseFeatures):
         # https://github.com/mongodb-labs/django-mongodb/issues/161
         "queries.tests.RelatedLookupTypeTests.test_values_queryset_lookup",
         "queries.tests.ValuesSubqueryTests.test_values_in_subquery",
-        # SQLCompiler.collection_name raises StopIteration
-        "model_inheritance_regress.tests.ModelInheritanceTest.test_mti_update_grand_parent_through_child",
-        "model_inheritance_regress.tests.ModelInheritanceTest.test_mti_update_parent_through_child",
     }
     # $bitAnd, #bitOr, and $bitXor are new in MongoDB 6.3.
     _django_test_expected_failures_bitwise = {