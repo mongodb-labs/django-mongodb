460a13692b09b623e14c014d3aef8108e714e6ac
Tim Graham <timograham@gmail.com>
2024-10-28 09:19:31 -0400

    add test for ObjectIdAutoField.deconstruct()

--- tests/model_fields_/test_autofield.py
+++ tests/model_fields_/test_autofield.py
@@ -4,6 +4,13 @@ from django_mongodb.fields import ObjectIdAutoField
 
 
 class MethodTests(SimpleTestCase):
+    def test_deconstruct(self):
+        field = ObjectIdAutoField()
+        name, path, args, kwargs = field.deconstruct()
+        self.assertEqual(path, "django_mongodb.fields.auto.ObjectIdAutoField")
+        self.assertEqual(args, [])
+        self.assertEqual(kwargs, {"db_column": "_id", "primary_key": True})
+
     def test_to_python(self):
         f = ObjectIdAutoField()
         self.assertEqual(f.to_python("1"), 1)
