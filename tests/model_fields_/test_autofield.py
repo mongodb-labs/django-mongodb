a06efe3582f008c3fdb2f944b189b0653da45666
Tim Graham <timograham@gmail.com>
2024-10-28 09:23:24 -0400

    implement ObjectIdAutoField.deconstruct()
    Remove "auto" from path and omit default db_column from kwargs.

--- tests/model_fields_/test_autofield.py
+++ tests/model_fields_/test_autofield.py
@@ -7,9 +7,9 @@ class MethodTests(SimpleTestCase):
     def test_deconstruct(self):
         field = ObjectIdAutoField()
         name, path, args, kwargs = field.deconstruct()
-        self.assertEqual(path, "django_mongodb.fields.auto.ObjectIdAutoField")
+        self.assertEqual(path, "django_mongodb.fields.ObjectIdAutoField")
         self.assertEqual(args, [])
-        self.assertEqual(kwargs, {"db_column": "_id", "primary_key": True})
+        self.assertEqual(kwargs, {"primary_key": True})
 
     def test_to_python(self):
         f = ObjectIdAutoField()
