e8a0d960e7ec170e8630bee9a53cde12e39ef9bc
Tim Graham <timograham@gmail.com>
2024-12-07 20:10:22 -0500

    add save/load tests modeled after UUIDField tests

--- tests/model_fields_/test_objectidfield.py
+++ tests/model_fields_/test_objectidfield.py
@@ -1,8 +1,10 @@
 from bson import ObjectId
-from django.core import exceptions
-from django.test import SimpleTestCase
+from django.core.exceptions import ValidationError
+from django.test import SimpleTestCase, TestCase
 
 from django_mongodb.fields import ObjectIdField
+
+from .models import NullableObjectIdModel, ObjectIdModel, PrimaryKeyObjectIdModel
 
 
 class MethodTests(SimpleTestCase):
@@ -33,7 +35,7 @@ class MethodTests(SimpleTestCase):
         for invalid_value in ["None", {}, [], 123]:
             with self.subTest(invalid_value=invalid_value):
                 msg = f"['“{invalid_value}” value must be an Object Id.']"
-                with self.assertRaisesMessage(exceptions.ValidationError, msg):
+                with self.assertRaisesMessage(ValidationError, msg):
                     f.to_python(invalid_value)
 
     def test_get_prep_value_string(self):
@@ -43,6 +45,12 @@ class MethodTests(SimpleTestCase):
     def test_get_prep_value_objectid(self):
         value = ObjectId("1" * 24)
         self.assertEqual(ObjectIdField().get_prep_value(value), value)
+
+    def test_get_prep_value_empty(self):
+        # This is necessary to allow an empty ObjectIdField to be saved in
+        # forms, unless we add an ObjectId form field to do the conversion (see
+        # UUIDField for an example).
+        self.assertIsNone(ObjectIdField().get_prep_value(""))
 
     def test_get_prep_value_null(self):
         self.assertIsNone(ObjectIdField().get_prep_value(None))
@@ -55,3 +63,36 @@ class MethodTests(SimpleTestCase):
                 msg = f"Field 'test' expected an ObjectId but got {invalid_value!r}."
                 with self.assertRaisesMessage(ValueError, msg):
                     f.get_prep_value(invalid_value)
+
+
+class SaveLoadTests(TestCase):
+    def test_objectid_instance(self):
+        instance = ObjectIdModel.objects.create(field=ObjectId())
+        loaded = ObjectIdModel.objects.get()
+        self.assertEqual(loaded.field, instance.field)
+
+    def test_str_instance(self):
+        ObjectIdModel.objects.create(field="6754ed8e584bc9ceaae3c072")
+        loaded = ObjectIdModel.objects.get()
+        self.assertEqual(loaded.field, ObjectId("6754ed8e584bc9ceaae3c072"))
+
+    def test_null_handling(self):
+        NullableObjectIdModel.objects.create(field=None)
+        loaded = NullableObjectIdModel.objects.get()
+        self.assertIsNone(loaded.field)
+
+    def test_pk_validated(self):
+        # See https://code.djangoproject.com/ticket/24859
+        with self.assertRaisesMessage(TypeError, "must be an Object Id."):
+            PrimaryKeyObjectIdModel.objects.get(pk={})
+
+        with self.assertRaisesMessage(TypeError, "must be an Object Id."):
+            PrimaryKeyObjectIdModel.objects.get(pk=[])
+
+    def test_wrong_value(self):
+        # Copied from  UUID tests. Raises ValueError, might be okay.
+        with self.assertRaisesMessage(ValidationError, "must be an Object Id."):
+            ObjectIdModel.objects.get(field="not-a-objectid")
+
+        with self.assertRaisesMessage(ValidationError, "must be an Object Id."):
+            ObjectIdModel.objects.create(field="not-a-objectid")
