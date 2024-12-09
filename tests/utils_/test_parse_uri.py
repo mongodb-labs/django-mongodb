040b870420e4dc3830bf13f1a28b163939fb3dba
Tim Graham <timograham@gmail.com>
2024-12-09 10:53:12 -0500

    test simplifications

--- tests/utils_/test_parse_uri.py
+++ tests/utils_/test_parse_uri.py
@@ -5,8 +5,6 @@ from django.test import SimpleTestCase
 
 from django_mongodb import parse_uri
 
-URI = "mongodb+srv://myDatabaseUser:D1fficultP%40ssw0rd@cluster0.example.mongodb.net/myDatabase?retryWrites=true&w=majority&tls=false"
-
 
 class ParseURITests(SimpleTestCase):
     def test_simple_uri(self):
@@ -16,65 +14,58 @@ class ParseURITests(SimpleTestCase):
         self.assertEqual(settings_dict["HOST"], "cluster0.example.mongodb.net")
 
     def test_no_database(self):
-        settings_dict = parse_uri("mongodb://cluster0.example.mongodb.net/")
-        self.assertEqual(settings_dict["ENGINE"], "django_mongodb")
+        settings_dict = parse_uri("mongodb://cluster0.example.mongodb.net")
         self.assertIsNone(settings_dict["NAME"])
         self.assertEqual(settings_dict["HOST"], "cluster0.example.mongodb.net")
 
-    # PyMongo will try to resolve the SRV record if the URI has the mongodb+srv:// prefix.
-    # https://pymongo.readthedocs.io/en/stable/api/pymongo/mongo_client.html#pymongo.mongo_client.MongoClient
     def test_srv_uri_with_options(self):
+        uri = "mongodb+srv://my_user:my_password@cluster0.example.mongodb.net/my_database?retryWrites=true&w=majority"
+        # patch() prevents  a crash when PyMongo attempts to resolve the
+        # nonexistent SRV record.
         with patch("dns.resolver.resolve"):
-            settings_dict = parse_uri(URI)
-            self.assertEqual(settings_dict["ENGINE"], "django_mongodb")
-            self.assertEqual(settings_dict["NAME"], "myDatabase")
-            self.assertEqual(settings_dict["HOST"], "mongodb+srv://cluster0.example.mongodb.net")
-            self.assertEqual(settings_dict["USER"], "myDatabaseUser")
-            self.assertEqual(settings_dict["PASSWORD"], "D1fficultP@ssw0rd")
-            self.assertIsNone(settings_dict["PORT"])
-            self.assertEqual(
-                settings_dict["OPTIONS"], {"retryWrites": True, "w": "majority", "tls": False}
-            )
+            settings_dict = parse_uri(uri)
+        self.assertEqual(settings_dict["NAME"], "my_database")
+        self.assertEqual(settings_dict["HOST"], "mongodb+srv://cluster0.example.mongodb.net")
+        self.assertEqual(settings_dict["USER"], "my_user")
+        self.assertEqual(settings_dict["PASSWORD"], "my_password")
+        self.assertIsNone(settings_dict["PORT"])
+        self.assertEqual(
+            settings_dict["OPTIONS"], {"retryWrites": True, "w": "majority", "tls": True}
+        )
 
     def test_localhost(self):
-        settings_dict = parse_uri("mongodb://localhost/myDatabase")
-        self.assertEqual(settings_dict["ENGINE"], "django_mongodb")
-        self.assertEqual(settings_dict["NAME"], "myDatabase")
-        self.assertEqual(settings_dict["HOST"], "localhost")
-
-    def test_localhost_with_port(self):
-        settings_dict = parse_uri("mongodb://localhost/myDatabase")
-        self.assertEqual(settings_dict["ENGINE"], "django_mongodb")
-        self.assertEqual(settings_dict["NAME"], "myDatabase")
+        settings_dict = parse_uri("mongodb://localhost")
         self.assertEqual(settings_dict["HOST"], "localhost")
         self.assertEqual(settings_dict["PORT"], 27017)
 
-    def test_localhosts_with_ports(self):
-        settings_dict = parse_uri(
-            "mongodb://localhost:27017,localhost:27018,localhost:27019/myDatabase"
-        )
-        self.assertEqual(settings_dict["ENGINE"], "django_mongodb")
-        self.assertEqual(settings_dict["NAME"], "myDatabase")
-        self.assertEqual(settings_dict["HOST"], "localhost:27017,localhost:27018,localhost:27019")
+    def test_localhost_with_port(self):
+        settings_dict = parse_uri("mongodb://localhost:27018")
+        self.assertEqual(settings_dict["HOST"], "localhost")
+        self.assertEqual(settings_dict["PORT"], 27018)
+
+    def test_hosts_with_ports(self):
+        settings_dict = parse_uri("mongodb://localhost:27017,localhost:27018")
+        self.assertEqual(settings_dict["HOST"], "localhost:27017,localhost:27018")
+        self.assertEqual(settings_dict["PORT"], None)
+
+    def test_hosts_without_ports(self):
+        settings_dict = parse_uri("mongodb://host1.net,host2.net")
+        self.assertEqual(settings_dict["HOST"], "host1.net:27017,host2.net:27017")
         self.assertEqual(settings_dict["PORT"], None)
 
     def test_conn_max_age(self):
-        settings_dict = parse_uri("mongodb://localhost/myDatabase", conn_max_age=600)
+        settings_dict = parse_uri("mongodb://localhost", conn_max_age=600)
         self.assertEqual(settings_dict["CONN_MAX_AGE"], 600)
 
     def test_test_kwarg(self):
-        settings_dict = parse_uri("mongodb://localhost/myDatabase", test={"NAME": "test_db"})
+        settings_dict = parse_uri("mongodb://localhost", test={"NAME": "test_db"})
         self.assertEqual(settings_dict["TEST"]["NAME"], "test_db")
 
     def test_invalid_credentials(self):
-        with self.assertRaises(pymongo.errors.InvalidURI):
-            parse_uri("mongodb://:@localhost/myDatabase")
+        msg = "The empty string is not valid username."
+        with self.assertRaisesMessage(pymongo.errors.InvalidURI, msg):
+            parse_uri("mongodb://:@localhost")
 
-    def test_no_prefix(self):
-        with self.assertRaises(pymongo.errors.InvalidURI):
-            parse_uri("cluster0.example.mongodb.net/myDatabase")
-
-    def test_hosts_without_ports(self):
-        settings_dict = parse_uri("mongodb://host1.net,host2.net/myDatabase")
-        self.assertEqual(settings_dict["HOST"], "host1.net:27017,host2.net:27017")
-        self.assertEqual(settings_dict["PORT"], None)
+    def test_no_scheme(self):
+        with self.assertRaisesMessage(pymongo.errors.InvalidURI, "Invalid URI scheme"):
+            parse_uri("cluster0.example.mongodb.net")
