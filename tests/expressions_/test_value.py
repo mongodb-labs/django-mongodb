f6ad5f01b6ca238f57af35d0c5b6516ef7e6257f
Tim Graham <timograham@gmail.com>
2025-01-01 09:35:15 -0500

    remove unnecessary $literal wrapping in Value.as_mql()
    It only needs to wrap integers, and wrapping over types causes incorrect
    matching in ArrayField.

--- tests/expressions_/test_value.py
+++ tests/expressions_/test_value.py
@@ -11,37 +11,33 @@ class ValueTests(SimpleTestCase):
     def test_date(self):
         self.assertEqual(
             Value(datetime.date(2025, 1, 1)).as_mql(None, None),
-            {"$literal": datetime.datetime(2025, 1, 1)},
+            datetime.datetime(2025, 1, 1),
         )
 
     def test_datetime(self):
         self.assertEqual(
             Value(datetime.datetime(2025, 1, 1, 9, 8, 7)).as_mql(None, None),
-            {"$literal": datetime.datetime(2025, 1, 1)},
+            datetime.datetime(2025, 1, 1),
         )
 
     def test_decimal(self):
-        self.assertEqual(Value(Decimal("1.0")).as_mql(None, None), {"$literal": Decimal128("1.0")})
+        self.assertEqual(Value(Decimal("1.0")).as_mql(None, None), Decimal128("1.0"))
 
     def test_time(self):
         self.assertEqual(
             Value(datetime.time(9, 8, 7)).as_mql(None, None),
-            {"$literal": datetime.datetime(1, 1, 1, 9, 8, 7)},
+            datetime.datetime(1, 1, 1, 9, 8, 7),
         )
 
     def test_timedelta(self):
-        self.assertEqual(
-            Value(datetime.timedelta(3600)).as_mql(None, None), {"$literal": 311040000000.0}
-        )
+        self.assertEqual(Value(datetime.timedelta(3600)).as_mql(None, None), 311040000000.0)
 
     def test_int(self):
         self.assertEqual(Value(1).as_mql(None, None), {"$literal": 1})
 
     def test_str(self):
-        self.assertEqual(Value("foo").as_mql(None, None), {"$literal": "foo"})
+        self.assertEqual(Value("foo").as_mql(None, None), "foo")
 
     def test_uuid(self):
         value = uuid.UUID(int=1)
-        self.assertEqual(
-            Value(value).as_mql(None, None), {"$literal": "00000000000000000000000000000001"}
-        )
+        self.assertEqual(Value(value).as_mql(None, None), "00000000000000000000000000000001")
