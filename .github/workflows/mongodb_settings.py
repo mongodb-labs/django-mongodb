ec978c5cb2731c2420dccb9393e1c84012e07cb2
Jib <jib.adegunloye@mongodb.com>
2025-01-23 08:03:36 -0500

    provided tls=true in auth/ssl uri

--- .github/workflows/mongodb_settings.py
+++ .github/workflows/mongodb_settings.py
@@ -5,6 +5,10 @@ from django_mongodb_backend import parse_uri
 
 PARSED_URI = parse_uri(os.getenv("MONGODB_URI")) if os.getenv("MONGODB_URI") else {}
 
+# Temporary fix for https://github.com/mongodb-labs/mongo-orchestration/issues/268
+if "USER" in PARSED_URI and "PASSWORD" in PARSED_URI:
+    PARSED_URI["OPTIONS"]["tls"] = True
+
 DATABASES = {
     "default": {
         **PARSED_URI,
@@ -17,7 +21,7 @@ DATABASES = {
         "NAME": "djangotests-other",
     },
 }
-print(DATABASES)
+
 DEFAULT_AUTO_FIELD = "django_mongodb_backend.fields.ObjectIdAutoField"
 PASSWORD_HASHERS = ("django.contrib.auth.hashers.MD5PasswordHasher",)
 SECRET_KEY = "django_tests_secret_key"
